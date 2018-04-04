a4fc6d286a8a01da540200acca1232255a803109
Cameron Button and Kristen Chellis <cameron@>
2018-04-04 13:57:26 -0700

    Debug computer player and add CSS styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 var player1;
 var player2;
 var choice;
+var name;
+var score;
+var winScore = 20;
 function Player(name){
   this.name = name;
   this.turnScore = 0;
@@ -35,6 +38,7 @@ Player.prototype.endTurn = function(){
 function computerTurn (){
   for (var i = 0; i < 3; i++) {
     player2.dieRoll();
+    console.log(player2.roll);
     var check = player2.checkRoll();
     if (!check) {
       i = 3;
@@ -52,15 +56,17 @@ $(function() {
     if ( choice === "computer") {
       player2= new Player ("HAL 9000")
       $(".computer").show();
+      name = "#name"
     }else {
       $(".human").show();
+      name = "#name1"
     }
     $("#choices").hide();
     $("#game").show();
   })
   $(".playerName").submit(function(event) {
     event.preventDefault();
-    player1 = new Player($(".name1").val());
+    player1 = new Player($(name).val());
     if ( choice !== "computer") {
       player2 = new Player($("#name2").val());
     }
@@ -69,44 +75,61 @@ $(function() {
     $(".active").prepend(activePlayer.name);
     $("#passive").prepend(passivePlayer.name);
     $("#hidden-game").show();
-    $("#playerName").hide();
-    $("#totalScore").text(activePlayer.totalScore);
+    $(".playerName").hide();
+    $("#totalScore1").text(activePlayer.totalScore);
+    $("#totalScore2").text(passivePlayer.totalScore);
     $(".option").click(function(){
-      if (activePlayer.name != "HAL 9000") {
-        if($(this).val()=== "roll") {
-          activePlayer.dieRoll();
-          $("#roll").text(activePlayer.roll);
-          var check = activePlayer.checkRoll();
-          $("#turnScore").text(activePlayer.turnScore);
-          $("#firstRoll").show();
-          if (!check) {
-            activePlayer.endTurn();
-            $("#totalScore").text(activePlayer.totalScore);
-            $("#roll1").show();
-            $("#nextTurn").show();
-            $("#turn").hide();
-          }
-        }else if ($(this).val()=== "hold") {
+      if($(this).val()=== "roll") {
+        activePlayer.dieRoll();
+        $("#roll").text(activePlayer.roll);
+        var check = activePlayer.checkRoll();
+        $("#turnScore").text(activePlayer.turnScore);
+        $("#firstRoll").show();
+        if (!check) {
           activePlayer.endTurn();
-          if (activePlayer.totalScore >= 20) {
-            $("#endGame").show();
-            $("#game").hide();
-          }
+          $("#totalScore1").text(activePlayer.totalScore);
+          $("#roll1").show();
+          $("#nextTurn").show();
+          $("#turn").hide();
         }
-      } else{
-        computerTurn();
-      }
-        $("#totalScore").text(activePlayer.totalScore);
+      }else if ($(this).val()=== "hold") {
+        activePlayer.endTurn();
+        if (activePlayer.totalScore >= winScore) {
+          $("#endGame").show();
+          $("#game").hide();
+        }
+        $("#totalScore1").text(activePlayer.totalScore);
         $("#endTurn").show();
         $("#nextTurn").show();
         $("#turn").hide();
+      }else if ($(this).val()=== "computer") {
+        $("#nextTurn").show();
+        $("#turn").hide();
+        $("#hal").hide();
+        computerTurn();
+        $("#totalScore1").text(activePlayer.totalScore);
+        if (activePlayer.totalScore >= winScore) {
+          $("#endGame").show();
+          $("#game").hide();
+        }
+      }
+
     });
     $("#nextTurn").click(function() {
-      $("#endTurn").hide();
-      $("#nextTurn").hide();
-      $("#roll1").hide();
-      $("#turn").show();
-      $("#firstRoll").hide();
+      if (choice === "computer" && activePlayer === player1) {
+        $("#endTurn").hide();
+        $("#nextTurn").hide();
+        $("#roll1").hide();
+        $("#turn").hide();
+        $("#firstRoll").hide();
+        $("#hal").show();
+      }else {
+        $("#endTurn").hide();
+        $("#nextTurn").hide();
+        $("#roll1").hide();
+        $("#turn").show();
+        $("#firstRoll").hide();
+      }
       var temp =activePlayer;
       activePlayer=passivePlayer;
       passivePlayer = temp;
