171af76510f5ce5fedbd251c744a88f98061e1ed
Cameron Button and Kristen Chellis <kristen@>
2018-04-04 15:51:05 -0700

    Debug hard AI

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,7 @@ var player2;
 var choice;
 var name;
 var totalScore;
-var winScore = 20;
-var apTotal = activePlayer.totalScore;
-var apTurn = activePlayer.turnScore;
+var winScore = 100;
 function Player(name){ //player object constructor
   this.name = name;
   this.turnScore = 0;
@@ -40,39 +38,46 @@ Player.prototype.endTurn = function(){ //ends the player's turn by adding the tu
 
 function computerTurn (){ //simple AI that will roll the die 3 times, unless one of those rolls is a one
   for (var i = 0; i < 3; i++) {
-    activePlayer.dieRoll();
-    var check = activePlayer.checkRoll();
+    player2.dieRoll();
+    var check = player2.checkRoll();
     if (!check) {
       i = 3;
     }
 
   }
-  activePlayer.endTurn();
-  return apTotal;
+  player2.endTurn();
+  return player2.totalScore;
 } //end of computerTurn
 
-function computerTurnHard () {
+function computerTurnHard () { //meaner AI
   var rollAgain;
   for (var i = 0; i < 100; i++) {
 
-    activePlayer.dieRoll();
-    var check = activePlayer.checkRoll();
-
-    if (apTotal < 70 && apTurn >= 20) {
+    player2.dieRoll();
+    console.log(player2.roll);
+    var check = player2.checkRoll();
+    if (!check) {
       i = 100;
-    } else if (passivePlayer.totalScore < 50 && apTurn >= 10) {
+      player2.turnScore = 0;
+    }
+    else if (player2.totalScore < 70 && player2.turnScore >= 20) {
       i = 100;
-    }  else if (apTotal + apTurn >= winScore){
+    } else if (player1.totalScore < 50 && player2.turnScore >= 10 && player2.totalScore >= 70) {
+      i = 100;
+    }  else if (player2.totalScore + player2.turnScore >= winScore){
       i = 100;
     }
   }
+  player2.endTurn();
+  return player2.totalScore;
+
 }
 
 //front end
 $(function() {
   $(".gameType").click(function() { //choose to play with a friend or the computer
     choice = $(this).val();
-    if ( choice === "computer") {
+    if (/computer/.test(choice)) {
       player2= new Player ("HAL 9000") //create a new player object for the computer
       $(".computer").show();
       name = "#name"
@@ -86,7 +91,7 @@ $(function() {
   $(".playerName").submit(function(event) { //submit the player names
     event.preventDefault();
     player1 = new Player($(name).val()); //create a player object for player1
-    if ( choice !== "computer") {
+    if (!/computer/.test(choice)) {
       player2 = new Player($("#name2").val()); //create a player object for player2 if they are not s computer
     }
     var activePlayer = player1;
@@ -95,7 +100,7 @@ $(function() {
     $(".p2").prepend(passivePlayer.name);
     $("#hidden-game").show();
     $(".playerName").hide();
-    $("#totalScore1").text(apTotal);
+    $("#totalScore1").text(activePlayer.totalScore);
     $("#totalScore2").text(passivePlayer.totalScore);
     $(".active").text(activePlayer.name);
     $(".option").click(function(){ //handles the choice of rolling or holding player score
@@ -109,24 +114,24 @@ $(function() {
         $("#runningScore").prepend("<li>" + activePlayer.roll + "</li>");
         $("#roll").text(activePlayer.roll);
         var check = activePlayer.checkRoll();
-        $(".turnScore").text(apTurn);
+        $(".turnScore").text(activePlayer.turnScore);
         $("#firstRoll").show();
         if (!check) {
           activePlayer.endTurn();
           $("#runningScore").text("");
           $("#runningScore").prepend("<li>Total: <span class ='turnScore'></span></li>");
-          $(totalScore).text(apTotal);
+          $(totalScore).text(activePlayer.totalScore);
           $("#roll1").show();
           $("#nextTurn").show();
           $("#turn").hide();
         }
       }else if ($(this).val()=== "hold") { //handles if the player chooses to hold
         activePlayer.endTurn();
-        if (apTotal >= winScore) { //checks active player's score to see if they win
+        if (activePlayer.totalScore >= winScore) { //checks active player's score to see if they win
           $("#endGame").show();
           $("#game").hide();
         }
-        $(totalScore).text(apTotal);
+        $(totalScore).text(activePlayer.totalScore);
         $("#endTurn").show();
         $("#nextTurn").show();
         $("#turn").hide();
@@ -134,9 +139,13 @@ $(function() {
         $("#nextTurn").show();
         $("#turn").hide();
         $("#hal").hide();
-        computerTurn();
-        $(totalScore).text(apTotal);
-        if (apTotal >= winScore) { //checks active player's score to see if they win
+        if (choice === "easycomputer") {
+          computerTurn();
+        }else{
+          computerTurnHard();
+        }
+        $(totalScore).text(activePlayer.totalScore);
+        if (activePlayer.totalScore >= winScore) { //checks active player's score to see if they win
           $("#endGame").show();
           $("#game").hide();
         }
@@ -144,7 +153,7 @@ $(function() {
 
     });
     $("#nextTurn").click(function() { //changes the active player
-      if (choice === "computer" && activePlayer === player1) {
+      if (/computer/.test(choice) && activePlayer === player1) {
         $("#endTurn").hide();
         $("#nextTurn").hide();
         $("#roll1").hide();
